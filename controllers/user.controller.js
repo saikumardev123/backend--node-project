276b232c02a1b98cc2b03c1bbdd2a089bd1da00d
sai <sai@digital-lync.com>
2022-01-24 07:29:15 +0530

    24Jan2022

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,38 @@
 
 const userModel = require('../model/user.model');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 exports.register = async (req,res) =>{
     var userDetails = req.body;
-    console.log("userDetails", userDetails);
-    var newUser =  new userModel(userDetails);
+    var salt = bcrypt.genSaltSync(10);
+     let user = {
+        "username": userDetails.username,
+        "password":  bcrypt.hashSync(userDetails.password,salt),
+        "emailId":userDetails.emailId,
+        "mobileNumber":userDetails.mobileNumber
+     }
+    console.log("userDetails", user);
+    var newUser =  new userModel(user);
     
      let resp = await userModel.find({username:userDetails.username});
      console.log("resp", resp);
-      if(resp[0]._id){
-          console.log("the user is already available in DB");
-          res.send({isRegistered: false, message: "username already available"});
-      }
-
+     if(resp.length > 1){
+        if(resp[0]._id){
+            console.log("the user is already available in DB");
+            res.send({isRegistered: false, message: "username already available"});
+        }
+    }
     try{
         let response =  await newUser.save();
          console.log(response);
          if(response._id){
-             res.send({isRegistered: true, message:"successfully registered!"});
+             var payload = { "subject": response._id }
+            let token= jwt.sign(payload,process.env.SECRET_KEY);
+             res.send({isRegistered: true, message:"successfully registered!", token:token});
          }
     }
     catch(error){
@@ -26,4 +41,40 @@ exports.register = async (req,res) =>{
          res.send({isRegistered: false, message: error.message});
     }
     
+}
+
+
+exports.login = async (req,res) => {
+
+    var userDetails = req.body; 
+
+    let resp = await userModel.find({emailId:userDetails.emailId});
+    if(resp.length > 0){
+        if(resp[0]._id){
+            
+             if(bcrypt.compareSync(userDetails.password,resp[0].password))
+             {   
+                var payload = { "subject": resp[0]._id }
+                let token= jwt.sign(payload,process.env.SECRET_KEY);
+                 res.send({isLoggedIn: true, message:"Login Sucess!", token:token});
+             }
+             else
+             {
+                 res.send("password is incorrect!!");
+             }
+
+            // console.log("the user is already available in DB");
+            // res.send({isRegistered: false, message: "username already available"});
+        }
+    }
+    else
+    {
+        res.send("emailid not registered!");
+    }
+
+}
+
+exports.changePassword = async (req,res) => {
+    console.log("I am in change password");
+    res.send("password changed");
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const dotenv = require('dotenv');
+var userModel = require('../model/user.model');
+dotenv.config();
 var userController = require("../controllers/user.controller");
+var jwt = require('jsonwebtoken');
 
-
+var authMiddleware = (req,res,next) => {
+        
+          var email = req.body.emailId;
+          console.log("email", email);
+          if(req.headers.authorization){
+                let token = req.headers.authorization.split(" ")[1];
+                jwt.verify(token,process.env.SECRET_KEY,function(err, payload){
+                    if(err){
+                        console.log("error", err);
+                        res.status(401).send(err.message);
+                    }
+                    if(payload){
+                        console.log(payload);
+                          
+                         userModel.find({emailId:email},function(err, result){
+                             console.log("Result", result);
+                             console.log("error", err);
+                               if(err){
+                                   res.send(err.message);
+                               }
+                               if(result.length > 0){
+                                     
+                                 if(result[0]._id == payload.subject){
+                                     next();
+                                 }
+                                 else{
+                                     res.status(401).send("unauthorized");
+                                 }
+                               }
+                               else{
+                                   res.status(401).send("unauthorized");
+                               }
+                         })
+                    }
+                })
+          }
+          else{
+              res.status(401).send("no token provided");
+          }
+}
+router.post("/changePassword",authMiddleware,userController.changePassword);
 router.post('/register',userController.register);
+router.post("/login", userController.login);
 module.exports = router;
